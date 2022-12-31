a31bce18886aaa6c3e0991ae977d4902e84c4dd0
chalie su <2331593669@.com>
2022-12-31 21:33:45 +0800

    练习

--- study02/src/number/Demo01.java
+++ study02/src/number/Demo01.java
@@ -7,15 +7,15 @@ public class Demo01 {
  //             int a = 12;
  //             int b = 20;
  //             System.out.println();
-                float a = 1.25f;
-                float b = 1.12f;
-                float c = a-b;
-                if(Math.abs(c)<0.135){
-                    System.out.println("计算有效");
-                }else{
-                    System.out.println("计算无效");
+               Character b = 'a';
+                System.out.println(Character.isLetter('7'));
+
+                String str = "zhalisu";
+                'a'-'z'
+                        'A'-'Z'
+                char a = ''
+
                 }
-                System.out.println(c);
             }
         }
 
